b2b1ed1290261920091412733b94866581a13949
Luno <2022585923@qq.com>
2026-02-16 17:04:23 +0800

    用户注册-定义接口

--- src/main/java/com/luno/echo/service/UserService.java
+++ src/main/java/com/luno/echo/service/UserService.java
@@ -9,5 +9,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
 * @createDate 2026-02-16 16:25:23
 */
 public interface UserService extends IService<User> {
-
+    /**
+     * 用户注册
+     *
+     * @param username      用户名
+     * @param password      用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id (返回 ID 比返回 boolean 更实用，前端可能需要拿着 ID 跳转)
+     */
+    long userRegister(String username, String password, String checkPassword);
 }
