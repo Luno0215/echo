39b0dd4a5805c8a295bd84c0a010ae6307fda2a2
Luno <2022585923@qq.com>
2026-02-16 18:53:49 +0800

    删除帖子-实现controller层代码

--- src/main/java/com/luno/echo/controller/PostController.java
+++ src/main/java/com/luno/echo/controller/PostController.java
@@ -4,6 +4,7 @@ import com.luno.echo.common.ErrorCode;
 import com.luno.echo.common.Result;
 import com.luno.echo.common.exception.BusinessException;
 import com.luno.echo.model.dto.PostAddRequest;
+import com.luno.echo.model.dto.PostDeleteRequest;
 import com.luno.echo.service.PostService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,20 @@ public class PostController {
         }
         long postId = postService.addPost(postAddRequest);
         return Result.ok(postId);
+    }
+
+    /**
+     * 删除帖子
+     * 对应需求: DELETE /post/delete
+     */
+    @DeleteMapping("/delete")
+    public Result<String> deletePost(@RequestBody PostDeleteRequest postDeleteRequest) {
+        if (postDeleteRequest == null || postDeleteRequest.getId() == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+
+        postService.deletePost(postDeleteRequest.getId());
+        return Result.ok("删除成功");
     }
 
 }
