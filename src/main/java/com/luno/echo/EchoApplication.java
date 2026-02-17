f193d3cc2ba17e2ad9188439a52d7242e770e975
Luno <2022585923@qq.com>
2026-02-17 20:40:58 +0800

    警告优化-删除启动类上的MapperScan注解，因为已在MP配置类上添加了

--- src/main/java/com/luno/echo/EchoApplication.java
+++ src/main/java/com/luno/echo/EchoApplication.java
@@ -1,11 +1,9 @@
 package com.luno.echo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.luno.echo.mapper")
 public class EchoApplication {
 
     public static void main(String[] args) {
