895ceed15bcb1f88747604d2df928c8c227cf308
Luno <2022585923@qq.com>
2026-02-17 11:43:58 +0800

    帖子点赞-定义点赞前缀Redis常量

--- src/main/java/com/luno/echo/common/constant/RedisConstants.java
+++ src/main/java/com/luno/echo/common/constant/RedisConstants.java
@@ -12,4 +12,10 @@ public class RedisConstants {
      * 只要用户在 30 分钟内有操作，就会自动刷新
      */
     public static final Long LOGIN_USER_TTL = 30L;
+
+    /**
+     * 帖子点赞的 Key 前缀
+     * 例子：echo:post:like:5a2b3c...
+     */
+    public static final String POST_LIKED_KEY = "echo:post:like:";
 }
